643ce3f383fb8d811f086064a590f8dcd910c867
oyasumipants <ibaraki_keigo@cyberagent.co.jp>
2025-01-03 20:57:48 +0900

    feat: package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -79,38 +80,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      body: Center(
+        child: Icon(FontAwesomeIcons.gift, color: Colors.teal),
       ),
-      body: Column(children: [
-        const Text("HelloWorld"),
-        const Text("ハローワールド"),
-        TextButton(
-          onPressed: () => {print("ボタンが押されたよ")},
-          child: const Text("テキストボタン"),
-        ),
-        Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: const [
-          Icon(
-            Icons.favorite,
-            color: Colors.pink,
-            size: 24.0,
-          ),
-          Icon(
-            Icons.audiotrack,
-            color: Colors.green,
-            size: 30.0,
-          ),
-          Icon(
-            Icons.beach_access,
-            color: Colors.blue,
-            size: 36.0,
-          ),
-        ]),
-      ]),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () => {print("押したね？")}, child: const Icon(Icons.timer)),
-      drawer: const Drawer(child: Center(child: Text("Drawer"))),
-      endDrawer: const Drawer(child: Center(child: Text("EndDrawer"))),
     );
   }
 }
