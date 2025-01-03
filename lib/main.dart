fd5d07de7ce69555dea592d8fadeb77c8dd16ebe
oyasumipants <ibaraki_keigo@cyberagent.co.jp>
2025-01-03 22:11:26 +0900

    fix: page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'test_page1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -56,45 +56,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool _flag = false;
-
-  // ボタンタップ時にフラグを切り替える
-  _click() async {
-    setState(() {
-      _flag = !_flag;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            AnimatedContainer(
-                duration: const Duration(seconds: 3),
-                width: _flag ? 100 : 50,
-                height: _flag ? 50 : 100,
-                padding:
-                    _flag ? const EdgeInsets.all(0) : const EdgeInsets.all(30),
-                margin:
-                    _flag ? const EdgeInsets.all(0) : const EdgeInsets.all(30),
-                transform: _flag ? Matrix4.skewX(0.0) : Matrix4.skewX(0.3),
-                color: _flag ? Colors.blue : Colors.grey),
-            AnimatedSwitcher(
-                duration: const Duration(seconds: 3),
-                child: _flag
-                    ? const Text("なにもない")
-                    : const Icon(Icons.favorite, color: Colors.pink))
-          ],
-        ),
-      ),
-      floatingActionButton:
-          FloatingActionButton(onPressed: _click, child: const Icon(Icons.add)),
-    );
+    return Scaffold(body: TestPage1());
   }
 }
