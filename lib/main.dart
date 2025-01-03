59bdb30fdf9f8352fd830473ce85b5159662317e
oyasumipants <ibaraki_keigo@cyberagent.co.jp>
2025-01-03 18:15:43 +0900

    fix: drawer

--- lib/main.dart
+++ lib/main.dart
@@ -79,35 +79,38 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(children: [
+        const Text("HelloWorld"),
+        const Text("ハローワールド"),
+        TextButton(
+          onPressed: () => {print("ボタンが押されたよ")},
+          child: const Text("テキストボタン"),
         ),
-        body: Column(children: [
-          const Text("HelloWorld"),
-          const Text("ハローワールド"),
-          TextButton(
-            onPressed: () => {print("ボタンが押されたよ")},
-            child: const Text("テキストボタン"),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: const [
+          Icon(
+            Icons.favorite,
+            color: Colors.pink,
+            size: 24.0,
           ),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                Icon(
-                  Icons.favorite,
-                  color: Colors.pink,
-                  size: 24.0,
-                ),
-                Icon(
-                  Icons.audiotrack,
-                  color: Colors.green,
-                  size: 30.0,
-                ),
-                Icon(
-                  Icons.beach_access,
-                  color: Colors.blue,
-                  size: 36.0,
-                ),
-              ]),
-        ]));
+          Icon(
+            Icons.audiotrack,
+            color: Colors.green,
+            size: 30.0,
+          ),
+          Icon(
+            Icons.beach_access,
+            color: Colors.blue,
+            size: 36.0,
+          ),
+        ]),
+      ]),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => {print("押したね？")}, child: const Icon(Icons.timer)),
+      drawer: const Drawer(child: Center(child: Text("Drawer"))),
+      endDrawer: const Drawer(child: Center(child: Text("EndDrawer"))),
+    );
   }
 }
